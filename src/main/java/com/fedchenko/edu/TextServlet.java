3a068d691cda6c48ea860a48de5cd0a4f949a29b
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2017-04-01 23:23:58 +0300

    add "request.getParam"

--- src/main/java/com/fedchenko/edu/TextServlet.java
+++ src/main/java/com/fedchenko/edu/TextServlet.java
@@ -16,20 +16,40 @@ public class TextServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
+    int limit;
+    String q = new String();
+    int length;
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try {
+            limit = Integer.parseInt(request.getParameter("limit"));
+        } catch (Exception ex) {
+            limit = 10000;
+            ex.printStackTrace();
+        }
+        try {
+            length = Integer.parseInt(request.getParameter("length"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        String q = request.getParameter("q");
 
-//        PrintWriter out = response.getWriter();
+        PrintWriter out = response.getWriter();
+        out.print("LIMIT: " + limit + " ");
+        out.print("QUERY: " + q + " ");
+        out.print("LENGTH: " + length + " ");
+
 
         ServletContext ctx = getServletContext();
         try (InputStream inputstrm = ctx.getResourceAsStream("/WEB-INF/testfile.txt")) {
-            System.out.print(inputstrm.hashCode());
+            out.print("HashCode of stream: " + inputstrm.hashCode() + " ");
+
+
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-
-
     }
 }
