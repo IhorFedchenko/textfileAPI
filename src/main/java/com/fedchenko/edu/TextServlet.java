adf8a964138e5b7629bb7d91d5e8200458975ce6
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2017-04-02 19:14:11 +0300

    At the output all text

--- src/main/java/com/fedchenko/edu/TextServlet.java
+++ src/main/java/com/fedchenko/edu/TextServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -29,27 +30,51 @@ public class TextServlet extends HttpServlet {
         } catch (Exception ex) {
             limit = 10000;
             ex.printStackTrace();
+
         }
         try {
             length = Integer.parseInt(request.getParameter("length"));
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        String q = request.getParameter("q");
 
-        PrintWriter out = response.getWriter();
-        out.print("LIMIT: " + limit + " ");
-        out.print("QUERY: " + q + " ");
-        out.print("LENGTH: " + length + " ");
-
-
+//        String q = request.getParameter("q");
         ServletContext ctx = getServletContext();
+        PrintWriter out = response.getWriter();
+
         try (InputStream inputstrm = ctx.getResourceAsStream("/WEB-INF/testfile.txt")) {
-            out.print("HashCode of stream: " + inputstrm.hashCode() + " ");
+//            out.print("HashCode of stream: " + inputstrm.hashCode() + " ");
+            String allText = new String(streamToString(inputstrm));
+            out.print(allText);
 
 
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+
+//        out.print("LIMIT: " + limit + " ");
+//        out.print("QUERY: " + q + " ");
+//        out.print("LENGTH: " + length + " ");
+
+    }
+
+    protected String streamToString(InputStream input) {
+        int limit = this.limit;
+        int count = 0;
+        try {
+            try (ByteArrayOutputStream result = new ByteArrayOutputStream()) {
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = input.read(buffer)) != -1) {
+                    result.write(buffer, 0, length);
+                }
+                return result.toString("UTF-8");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 }
